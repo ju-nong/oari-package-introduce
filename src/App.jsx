88e645ff56cbc0fb3ebf1b01881b137c419c2421
이준용 <iiiuooyoo@gmail.com>
2023-05-09 16:09:53 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import oari from "oari";
+
+function App() {
+    const fetchImage = async () => {
+        const image = await oari();
+
+        setImages(image);
+    };
+
+    const [images, setImages] = useState({
+        filename: "",
+        file: null,
+    });
+
+    useEffect(() => {
+        fetchImage();
+    }, []);
+
+    return (
+        <div className="App">
+            <a href="https://www.npmjs.com/package/oari" target="_blank">
+                <h1>npm oari package</h1>
+            </a>
+            어서오고
+            {images.file === null ? (
+                <></>
+            ) : (
+                <>
+                    <img
+                        key={images.filename}
+                        src={URL.createObjectURL(images.file)}
+                        alt={images.filename}
+                        onClick={fetchImage}
+                    />
+                    <p>{images.filename}</p>
+                </>
+            )}
+            <p>Click Image</p>
+        </div>
+    );
+}
+
+export default App;
